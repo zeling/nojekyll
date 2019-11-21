6ccfef1964ddab4080dfcf0c948ea13b37cd0c29
Zeling Feng <zeling_feng@brown.edu>
2019-11-20 20:36:41 -0500

    HTML files

--- posts/second.rkt
+++ posts/second.rkt
@@ -3,8 +3,8 @@
 (require "../template.rkt")
 
 ; deliberately ignore the title
-; (provide title)
-; (define title "Second post") 
+(provide title)
+(define title "Second post") 
 
 @render{
 This is going to be the second post!
