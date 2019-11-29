f5a6ef2ec9727fe0412811bf62ab10255e6a4c26
Zeling Feng <zeling_feng@brown.edu>
2019-11-29 01:11:18 -0500

    fixed post order in index.rkt

--- index.rkt
+++ index.rkt
@@ -2,6 +2,7 @@
 
 (require file/glob
          racket/system
+         racket/date
          "template.rkt")
 
 (struct post (name title time))
@@ -16,14 +17,22 @@
         (with-output-to-file "/dev/null" #:exists 'append
             (lambda () (dynamic-require path sym)))))
 
+(define (string->seconds str)
+    (let ([parts (map string->number (string-split str "-"))])
+        (apply find-seconds `(0 0 0 ,@(reverse parts)))))
+
 (define (posts)
     (define (to-name p)
         (path->string (file-name-from-path p)))
-    (for/list ([path (glob "posts/*.rkt")])
-        (let ([title (get-attr path 'title "No title")]
-              [time ($ "git" "log" "-n" "1" "--pretty=%ad" "--date" "short" (path->string path))]
-              [name (to-name path)])
-        (post name title (string-trim time)))))
+    (define (post-order p1 p2)
+        (< (string->seconds (post-time p1)) (string->seconds (post-time p2))))
+    (let ([posts (for/list ([path (glob "posts/*.rkt")])
+            (let ([title (get-attr path 'title "No title")]
+                [time ($ "git" "log" "-n" "1" "--pretty=%ad" "--date" "short" (path->string path))]
+                [name (to-name path)])
+            (post name title (string-trim time))))])
+        (sort posts post-order)))
+
 
 (define (post-link p)
     (define (post-link name)
