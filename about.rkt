c3bc323171ccad3c226b2080fd135f654426c37e
Zeling Feng <zeling_feng@brown.edu>
2019-11-30 16:22:37 -0500

    UPDATE: about page, 3rd revise

--- about.rkt
+++ about.rkt
@@ -12,7 +12,7 @@
     @p{
         I am not a web designer, so please bear with me for the terrible web design. I tried to make the content more readable, the CSS stylesheet 
         of this site is based on (blatantly copied from) @a[href: "http://bettermotherfuckingwebsite.com/"]{this site}. The good news is that unlike
-        some other sites, I won't add any random particle effects whose meaning remains unclear.
+        some other sites, I won't add any suspicious particle effects whose meaning remains unclear.
     }
     @p{
         Opinions expressed are solely my own and do not express the views or opinions of my employer.
